5966bdda4d65363d4621b2ac5facaca6ff1942b5
Henrique <henrique.maltauro@edu.unipar.br>
2021-05-29 10:56:32 -0300

    Versao aula 2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TableTodoComponent } from './table-todo/table-todo.component';
+import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
+import { ToastrModule } from 'ngx-toastr';
+import { MessageValidControlComponent } from './shared/components/message-valid-control/message-valid-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableTodoComponent,
+    FormUsuarioComponent,
+	 MessageValidControlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
